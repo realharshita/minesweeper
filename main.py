526940cbadf5367ec7dc166475a222ae6b17e1c9
realharshita <slayyharshita@gmail.com>
2024-07-03 02:26:03 +0530

    logic

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class MinesweeperGame:
         
         self.create_widgets()
         self.place_mines()
+        self.calculate_adjacent_mines()
 
     def create_widgets(self):
         self.board_frame = tk.Frame(self.master)
@@ -30,13 +31,41 @@ class MinesweeperGame:
         while mines_placed < self.mines:
             r = random.randint(0, self.rows - 1)
             c = random.randint(0, self.cols - 1)
-            if self.board[r][c] != -1:  # if no mine
-                self.board[r][c] = -1  # mine
+            if self.board[r][c] != -1:
+                self.board[r][c] = -1
                 mines_placed += 1
     
-    def on_click(self, r, c):
+    def calculate_adjacent_mines(self):
+        directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        for r in range(self.rows):
+            for c in range(self.cols):
+                if self.board[r][c] == -1:
+                    continue
+                mine_count = 0
+                for dr, dc in directions:
+                    rr, cc = r + dr, c + dc
+                    if 0 <= rr < self.rows and 0 <= cc < self.cols and self.board[rr][cc] == -1:
+                        mine_count += 1
+                self.board[r][c] = mine_count
 
-        pass
+    def on_click(self, r, c):
+        if self.board[r][c] == -1:
+            self.buttons[r][c].config(text='*', bg='red')
+            self.reveal_mines()
+        else:
+            self.reveal_cell(r, c)
+
+    def reveal_cell(self, r, c):
+        if self.board[r][c] == 0:
+            self.buttons[r][c].config(text='', bg='light grey', state='disabled')
+        else:
+            self.buttons[r][c].config(text=str(self.board[r][c]), bg='light grey', state='disabled')
+
+    def reveal_mines(self):
+        for r in range(self.rows):
+            for c in range(self.cols):
+                if self.board[r][c] == -1:
+                    self.buttons[r][c].config(text='*', bg='red')
 
 def main():
     root = tk.Tk()
